822da83326524eeaa73d5ab6cc2601b0b778a1b9
Jan Wieck <janwieck@yahoo.com>
2013-02-17 11:02:01 -0500

    Finish thermistor example.

--- Examples/thermistor/thermistor.c
+++ Examples/thermistor/thermistor.c
@@ -1,11 +1,27 @@
-/*
+/* ----------------------------------------------------------------------
+ * thermistor.c
  *
+ *	Example program demonstrating how to measure temperature with
+ *	a thermistor.
+ * ----------------------------------------------------------------------
  */
 
 
 #include <stdio.h>
 
 #include "open8055.h"
+
+/* ----
+ * The following values are from the thermistor datasheet.
+ *
+ *	Beta in this case is the R/T characteristic of a
+ *	B57560G 10K NTC thermistor.
+ *	R1 is the nominal resistance at T1 degrees Celsius.
+ * ----
+ */
+#define	Beta		3390
+#define R1			10000
+#define	T1			25
 
 
 int
@@ -14,9 +30,18 @@ main(int argc, char *argv[])
 	char	*destination = "card0";
 	int		card;
 
+	/* ----
+	 * The default is "card0". A different Open8055 card can
+	 * be specified on the command line.
+	 * ----
+	 */
 	if (argc > 1)
 		destination = argv[1];
 
+	/* ----
+	 * Try to connect to the Open8055.
+	 * ----
+	 */
 	card = Open8055_Connect(destination, NULL);
 	if (card < 0)
 	{
@@ -24,15 +49,50 @@ main(int argc, char *argv[])
 		return 2;
 	}
 
+	/* ----
+	 * 8 bit ADC resolution is all we need for this example.
+	 * ----
+	 */
 	Open8055_SetModeADC(card, 0, OPEN8055_MODE_ADC8);
 
+	/* ----
+	 * Loop until the user aborts the program with CTRL-C.
+	 * ----
+	 */
 	for(;;)
 	{
-		printf("ADC1=%d     \r", Open8055_GetADC(card, 0));
+		double	R2;
+		double	ADCvalue;
+		double	T1kelvin;
+		double	T2kelvin;
+
+		/* ----
+		 * Measure the current resistance of the thermistor.
+		 * ----
+		 */
+		ADCvalue = (double)Open8055_GetADC(card, 0) / 256.0;
+		R2 = 1.0 / ((ADCvalue / 23 * 11) / 3900.0) - 3900.0;
+
+		/* ----
+		 * Knowing that we can calculate the temperature.
+		 * ----
+		 */
+		T1kelvin = T1 + 273.15;
+		T2kelvin = T1kelvin * Beta / log(R1/R2) / 
+					(Beta / log(R1/R2) - T1kelvin);
+
+		printf("\r%5.1f C %5.1f F", T2kelvin - 273.15,
+					(T2kelvin - 273.15) / 5.0 * 9.0 + 32.0);
 		fflush(stdout);
 
+		usleep(1000000);
 		if (Open8055_Wait(card) < 0)
+		{
+			printf("\r                   \r");
+			fflush(stdout);
+			fprintf(stderr, "%s: %s\n", destination, Open8055_LastError(card));
 			break;
+		}
 	}
 
 	fprintf(stderr, "%s: %s\n", destination, Open8055_LastError(card));
@@ -40,3 +100,5 @@ main(int argc, char *argv[])
 
 	return 0;
 }
+
+
