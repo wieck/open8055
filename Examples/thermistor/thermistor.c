49218efe775921e260524498322415fb1edec182
Jan Wieck <janwieck@yahoo.com>
2013-02-17 00:23:58 -0500

    Connecting and reading the ADC1 so far

--- Examples/thermistor/thermistor.c
+++ Examples/thermistor/thermistor.c
@@ -11,6 +11,32 @@
 int
 main(int argc, char *argv[])
 {
-	printf("hello world\n");
+	char	*destination = "card0";
+	int		card;
+
+	if (argc > 1)
+		destination = argv[1];
+
+	card = Open8055_Connect(destination, NULL);
+	if (card < 0)
+	{
+		fprintf(stderr, "%s: %s\n", destination, Open8055_LastError(-1));
+		return 2;
+	}
+
+	Open8055_SetModeADC(card, 0, OPEN8055_MODE_ADC8);
+
+	for(;;)
+	{
+		printf("ADC1=%d     \r", Open8055_GetADC(card, 0));
+		fflush(stdout);
+
+		if (Open8055_Wait(card) < 0)
+			break;
+	}
+
+	fprintf(stderr, "%s: %s\n", destination, Open8055_LastError(card));
+	Open8055_Close(card);
+
 	return 0;
 }
